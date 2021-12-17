50197381f2858e072cbd78d9e73e72273f8a8bdb
AshryA <ahmed.ashry@vodafone.com>
2021-12-17 20:45:04 +0200

    feat: add colors and fonts theme

---
+++ src/Theme/Colors.js
@@ -0,0 +1,9 @@
+const Colors = {
+  white: '#fff',
+  black: '#000',
+  frenchBlue: 'rgb(66,103,178)',
+  lightGrey: '#666666',
+  green: '#13e000'
+}
+
+export default Colors
---
+++ src/Theme/Fonts.js
@@ -0,0 +1,8 @@
+const Fonts = {
+  small: 12,
+  medium: 14,
+  large: 18,
+  brandSize: 40
+}
+
+export default Fonts
