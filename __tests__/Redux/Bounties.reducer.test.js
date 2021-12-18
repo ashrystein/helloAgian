ab2eaeec4fa44db78ce79a01e4e033116cbc2a31
AshryA <ahmed.ashry@vodafone.com>
2021-12-18 19:37:10 +0200

    enhance: unit testing for bounties reduce

--- __tests__/Redux/Bounties.reducer.test.js
+++ __tests__/Redux/Bounties.reducer.test.js
@@ -1,6 +1,6 @@
 import store from '../../src/Redux/Store'
 
-import { bountiesActions } from '../../src/Redux/Reducers/Bounties/slice'
+import { bountiesActions } from '../../src/Redux/Reducers/Bounties'
 
 const bountiesStub = {
   data: [
@@ -11,26 +11,31 @@ const bountiesStub = {
   ],
   status: 200
 }
+
+jest.mock('../../src/Services/Apis', () => {
+  return {
+    getBounties: jest.fn(() => bountiesStub)
+  }
+})
+
 describe('Bounties reducer', () => {
   it('should set isLoading to (true) and error to (undefined)', () => {
-    store.dispatch(bountiesActions.fetchStart())
+    store.dispatch(bountiesActions.fetchBounties())
     const { isLoading, error } = store.getState().bountiesReducer
     expect(isLoading).toBe(true)
     expect(error).toBe(undefined)
   })
 
   it('should get bounties successfully and errot to (undefined) and isLoading to false', () => {
-    store.dispatch(bountiesActions.fetchSuccess(bountiesStub))
-    const { isLoading, error, bounties } = store.getState().bountiesReducer
-    expect(bounties).toEqual(bountiesStub.data)
-    expect(isLoading).toBe(false)
-    expect(error).toBe(undefined)
-  })
-
-  it('should set errot to (error) and isLoading to false', () => {
-    store.dispatch(bountiesActions.fetchError({ error: 'error' }))
-    const { isLoading, error } = store.getState().bountiesReducer
-    expect(isLoading).toBe(false)
-    expect(error).toEqual({ error: 'error' })
+    store.dispatch(bountiesActions.fetchBounties())
+    return new Promise((resolve) => {
+      setTimeout(() => {
+        const { isLoading, error, bounties } = store.getState().bountiesReducer
+        expect(bounties).toEqual(bountiesStub.data)
+        expect(isLoading).toBe(false)
+        expect(error).toBe(undefined)
+        resolve(1)
+      }, 1000)
+    })
   })
 })
