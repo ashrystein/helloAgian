b8175c6a770196217b984dd96158945c1dc7a1da
AshryA <ahmed.ashry@vodafone.com>
2021-12-18 10:11:15 +0200

    feat: add shared styles to be reusable

--- src/Theme/index.js
+++ src/Theme/index.js
@@ -2,5 +2,6 @@ import Fonts from './Fonts'
 import Colors from './Colors'
 import Images from './Images'
 import Metrics from './Metrics'
+import Styles from './Styles'
 
-export { Fonts, Colors, Images, Metrics }
+export { Fonts, Colors, Images, Metrics, Styles }
