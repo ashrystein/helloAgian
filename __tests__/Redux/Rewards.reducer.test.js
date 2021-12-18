d2d2c6b45ed37187ca9be14113269680b0bad708
AshryA <ahmed.ashry@vodafone.com>
2021-12-18 11:39:30 +0200

    test: add rewards reducer unit testing

---
+++ __tests__/Redux/Rewards.reducer.test.js
@@ -0,0 +1,26 @@
+import store from '../../src/Redux/Store'
+
+import { rewardsActions } from '../../src/Redux/Reducers/Rewards'
+
+const rewardStub = [
+  { id: 1, name: 'reward one' },
+  { id: 2, name: 'reward two' }
+]
+describe('Rewards reducer', () => {
+  beforeEach(() => {
+    store.dispatch(rewardsActions.collectReward(rewardStub[0]))
+  })
+  it('should add new reward to rewards list', () => {
+    store.dispatch(rewardsActions.collectReward(rewardStub[0]))
+    store.dispatch(rewardsActions.collectReward(rewardStub[1]))
+    const { rewards } = store.getState().rewardsReducer
+    expect(rewards.length).toBe(2)
+  })
+
+  it('should not add existing reward to rewards list', () => {
+    store.dispatch(rewardsActions.collectReward(rewardStub[0]))
+    const { rewards } = store.getState().rewardsReducer
+    console.log(rewards)
+    expect(rewards.length).not.toBe(3)
+  })
+})
